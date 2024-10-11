32939cfea62ce55d5b9ac84e063d24c682572cb1
Vinuki Amarasinghe <vinukiiamare02@gmail.com>
2024-10-11 08:00:25 +0530

    styling header for home page

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -40,6 +40,7 @@ const Navbar = () => {
                 ? <div className='flex items-center gap-2 cursor-pointer group relative'>
                     <img src={assets.profile_pic} alt="" className='w-8 rounded-full cursor-pointer' onClick={()=>navigate('/my-profile')}/>
                     <img src={assets.dropdown_icon} alt="" className='w-2.5'/>
+                    
                     <div className='absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block'>
                         <div className='min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4'>
                             <p onClick={()=>navigate('/my-profile')} className='hover:text-black cursor-pointer'>My Profile</p>
